5067b1c8974ea43114205479e22c104d21a9f193
Chris Blakely <chrisblakely91@gmail.com>
2017-04-16 18:51:51 +0100

    Added Remove Minimum From List Kata (& Linq Solution)

---
+++ CodeKata/RemoveTheMinimum/Kata.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveTheMinimum
+{
+    public class Kata
+    {
+        public static List<int> RemoveSmallest(List<int> numbers)
+        {
+            int smallest = Int32.MaxValue;
+
+            foreach(var value in numbers)
+            {
+                if (value < smallest)
+                    smallest = value;
+            }
+
+            numbers.Remove(smallest);
+
+            return numbers;
+        }
+
+        public static List<int> RemoveSmallestLinq(List<int> numbers)
+        {
+            numbers.Remove(numbers.DefaultIfEmpty().Min());
+            return numbers;
+        }
+    }
+}
