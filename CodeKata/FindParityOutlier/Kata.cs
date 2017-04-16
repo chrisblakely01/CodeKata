d10f8495e97ae68b6256f5dc702abb04d3d5e7f2
Chris Blakely <chrisblakely91@gmail.com>
2017-04-16 18:36:21 +0100

    Added Find The Parity Outlier kata

---
+++ CodeKata/FindParityOutlier/Kata.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+public class Kata
+{
+    public static int Find(int[] integers)
+    {
+        //at least 2 values must be odd/even so we check the first 3
+        int firstThree = integers[0] % 2 + integers[1] % 2 + integers[2] % 2;
+
+        int rem = firstThree < 2 ? 1 : 0;
+
+        for (int i = 0; i < integers.Length; i++)
+        {
+            if (integers[i] % 2 == rem)
+            {
+                return integers[i];
+            }
+        }
+        return -1;
+    }
+}
